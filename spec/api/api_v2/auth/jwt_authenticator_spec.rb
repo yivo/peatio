89a65b878d635041f1e67d31adff646a58ec9f4f
Yaroslav Konoplov <eahome00@gmail.com>
2017-12-06 19:55:34 +0200

    Add specs for JWT

---
+++ spec/api/api_v2/auth/jwt_authenticator_spec.rb
@@ -0,0 +1,29 @@
+describe APIv2::Auth::JWTAuthenticator do
+  let :token do
+    'Bearer ' + JWT.encode(payload, APIv2::Auth::Utils.jwt_shared_secret_key, 'RS256')
+  end
+
+  let :endpoint do
+    stub('endpoint', options: { route_options: { scopes: ['identity'] } })
+  end
+
+  let :request do
+    stub 'request', \
+      request_method: 'GET',
+      path_info:      '/members/me',
+      env:            { 'api.endpoint' => endpoint },
+      headers:        { 'Authorization' => token }
+  end
+
+  let :member do
+    create(:member)
+  end
+
+  let :payload do
+    { x: 'x', y: 'y', z: 'z', member_id: member.id }
+  end
+
+  subject { APIv2::Auth::JWTAuthenticator.new(request.headers['Authorization']) }
+
+  it { expect(subject.authenticate!).to eq member }
+end
