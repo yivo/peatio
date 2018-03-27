54b4db9d46b025d300e36c0229d07ed6e56c7f74
Yaroslav Konoplov <eahome00@gmail.com>
2018-03-27 11:41:23 +0300

    Replace public? => private?

--- config/initializers/jwt.rb
+++ config/initializers/jwt.rb
@@ -1,5 +1,5 @@
 unless ENV['JWT_PUBLIC_KEY'].blank?
-  unless APIv2::Auth::Utils.jwt_public_key.public?
+  if APIv2::Auth::Utils.jwt_public_key.private?
     raise ArgumentError, 'JWT_PUBLIC_KEY was set to private key, however it should be public.'
   end
 end
@@ -10,7 +10,7 @@ require 'openssl'
 (YAML.load_file('config/management_api_v1.yml') || {}).deep_symbolize_keys.tap do |x|
   x.fetch(:keychain).each do |id, key|
     key = OpenSSL::PKey.read(Base64.urlsafe_decode64(key.fetch(:value)))
-    unless key.public?
+    if key.private?
       raise ArgumentError, 'keychain.' + id + ' was set to private key, ' \
                            'however it should be public (in config/management_api_v1.yml).'
     end
