a2eedaa1e90d208a6437d526d8fe9c4bfa61b1ea
Yaroslav Konoplov <eahome00@gmail.com>
2018-04-06 12:09:24 +0300

    Up!

--- spec/api/api_v2/withdraws_spec.rb
+++ spec/api/api_v2/withdraws_spec.rb
@@ -1,12 +1,10 @@
 describe APIv2::Withdraws, type: :request do
   let(:member) { create(:member, :verified_identity) }
   let(:token) { jwt_for(member) }
-
   let(:unverified_member) { create(:member, :unverified) }
   let(:unverified_member_token) { jwt_for(unverified_member) }
-
-  let(:btc_withdraws) { btc_withdraw_destinations.map { |address| create(:btc_withdraw, member: member, destination_id: address.id) } }
-  let(:usd_withdraws) { usd_withdraw_destinations.map { |address| create(:usd_withdraw, member: member, destination_id: address.id) } }
+  let(:btc_withdraws) { create(:btc_withdraw, 20, member: member) }
+  let(:usd_withdraws) { create(:usd_withdraw, 20, member: member) }
 
   before do
     # Force evaluate all.
@@ -74,27 +72,27 @@ describe APIv2::Withdraws, type: :request do
 
   describe 'POST /api/v2/withdraws' do
     it 'should validate withdraw amount' do
-      api_post '/api/v2/withdraws', params: { currency: 'btc', destination_id: btc_withdraw_destinations.first.id, amount: 'invalid' }, token: token
+      api_post '/api/v2/withdraws', params: { currency: 'btc', rid: Faker::Bitcoin.address, amount: 'invalid' }, token: token
       expect(response.code).to eq '422'
     end
 
     it 'should validate currency code' do
-      api_post '/api/v2/withdraws', params: { currency: 'invalid', destination_id: btc_withdraw_destinations.first.id, amount: '1' }, token: token
+      api_post '/api/v2/withdraws', params: { currency: 'invalid', rid: Faker::Bitcoin.address, amount: '1' }, token: token
       expect(response.code).to eq '422'
     end
 
     it 'should create withdraw using downcase currency code' do
-      api_post '/api/v2/withdraws', params: { currency: 'btc', destination_id: btc_withdraw_destinations.first.id, amount: '1' }, token: token
+      api_post '/api/v2/withdraws', params: { currency: 'btc', rid: Faker::Bitcoin.address, amount: '1' }, token: token
       expect(response.code).to eq '201'
     end
 
     it 'should create withdraw using upcase currency code' do
-      api_post '/api/v2/withdraws', params: { currency: 'BTC', destination_id: btc_withdraw_destinations.first.id, amount: '1' }, token: token
+      api_post '/api/v2/withdraws', params: { currency: 'BTC', rid: Faker::Bitcoin.address, amount: '1' }, token: token
       expect(response.code).to eq '201'
     end
 
     it 'should allow to create withdraw where amount is fraction number' do
-      api_post '/api/v2/withdraws', params: { currency: 'BTC', destination_id: btc_withdraw_destinations.first.id, amount: '0.1' }, token: token
+      api_post '/api/v2/withdraws', params: { currency: 'BTC', rid: Faker::Bitcoin.address, amount: '0.1' }, token: token
       expect(response.code).to eq '201'
       expect(JSON.parse(response.body)['amount'].to_d).to eq '0.1'.to_d
     end
