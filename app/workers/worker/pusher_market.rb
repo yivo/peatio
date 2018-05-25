552d50d9381e3eab2fda4516864e98ca2aab4112
Yaroslav Konoplov <eahome00@gmail.com>
2018-05-25 19:19:07 +0300

    Up

--- app/workers/worker/pusher_market.rb
+++ app/workers/worker/pusher_market.rb
@@ -5,8 +5,8 @@ module Worker
   class PusherMarket
     def process(payload)
       trade = Trade.new(payload)
-      Pusher["private-#{trade.ask.member.sn}"].trigger(:order, trade.for_notify('ask'))
-      Pusher["private-#{trade.bid.member.sn}"].trigger(:order, trade.for_notify('bid'))
+      Pusher["private-#{trade.ask.member.sn}"].trigger(:trade, trade.for_notify('ask'))
+      Pusher["private-#{trade.bid.member.sn}"].trigger(:trade, trade.for_notify('bid'))
       Pusher["market-#{trade.market.id}-global"].trigger(:trades, trades: [trade.for_global])
     end
   end
